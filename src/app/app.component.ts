60882410451dcb636298be327ca875a8dd98b503
minu <minubenny96@gmail.com>
2023-04-05 01:43:20 -0700

    Initial Commit for the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from './services/auth.service';
+import { User } from './models/user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NgDotNetAuth.UI';
-}
+  user = new User();
+  constructor (private authService: AuthService)
+  {}
+  register(user: User){
+    this.authService.register(user).subscribe();
+  }
+  login(user: User){
+    this.authService.login(user).subscribe((token: string) => { 
+      localStorage.setItem('authToken' , token);
+    });
+  }
+    getme(){
+      this.authService.getMe().subscribe((name : string) => {
+        console.log(name);
+      });
+
+    }
+  }
+
+
